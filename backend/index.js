dbb185f36c1959476ab660406fc118f9928ebcc4
Moee5055 <kshitij.moee55@gmail.com>
2025-03-13 10:12:05 +0545

    added dev script and port env variable

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import { PrismaClient } from '@prisma/client';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const prisma = new PrismaClient();
 
 app.use(express.json());
