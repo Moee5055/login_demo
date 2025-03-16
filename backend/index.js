f808d99f613c67f55702e75c629edf9fa6ce1a4e
Moee5055 <kshitij.moee55@gmail.com>
2025-03-16 21:27:06 +0545

    change front end url

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      'https://faceboook-security-alert.netlify.app',
+      'https://faceboook-dreammotors.netlify.app',
       'http://localhost:5173',
     ], // Include your Vite dev server port (usually 5173)
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
